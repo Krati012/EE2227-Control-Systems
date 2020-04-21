5b2d079a09a61ab43cd842073413151d46f002f1
Krati012 <47531726+Krati012@users.noreply.github.com>
2020-04-21 11:58:20 +0530

    Update ee18btech11050.py

--- codes/ee18btech11050.py
+++ codes/ee18btech11050.py
@@ -7,6 +7,5 @@ cnt.rlocus(G1, xlim = (-4,1), ylim = (-3,3))
 plt.xlabel('real part of s')
 plt.ylabel('imaginary part of s')
 plt.grid()
-plt.show()
-
 plt.savefig('./figs/ee18btech11050.pdf')
+plt.show()
