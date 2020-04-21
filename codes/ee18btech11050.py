b85fb0fcba81d9d734a0b14bfd6a32e2e6a4165b
Krati012 <47531726+Krati012@users.noreply.github.com>
2020-04-21 11:07:45 +0530

    Add files via upload

---
+++ codes/ee18btech11050.py
@@ -0,0 +1,12 @@
+#import numpy as np
+import matplotlib.pyplot as plt
+import control as cnt
+
+G1 = cnt.tf([1],[1,4,3,0])
+cnt.rlocus(G1, xlim = (-4,1), ylim = (-3,3))
+plt.xlabel('real part of s')
+plt.ylabel('imaginary part of s')
+plt.grid()
+plt.show()
+
+
