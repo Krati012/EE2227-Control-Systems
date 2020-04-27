dfe03560dcfc7db5ffccb0d82d14f56362e174ec
Krati012 <47531726+Krati012@users.noreply.github.com>
2020-04-28 00:23:44 +0530

    Update ee18btech11050_1.py

--- assignment2/codes/ee18btech11050_1.py
+++ assignment2/codes/ee18btech11050_1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr 26, 21:06:20 2020
+Created on Mon Apr 26 21:06:20 2020
 
 @author: krati
 """
@@ -15,21 +15,26 @@ import subprocess
 import shlex
 #end if
 
-zeros = []  #zeros of transfer function
-poles = [0,-3,-6] #poles of tf
+zeros = []
+poles = [0,-3,-6]
 
-H_s = signal.ZerosPolesGain(zeros, poles, [50])  #feed zeros and poles as inputs to this function to get H_s
-omega= np.linspace(-100, 100,20000) #range of values over which H is taken
+s1 = signal.ZerosPolesGain(zeros, poles, [50])
+omega= np.linspace(-100, 100,20000)
 
-w, H = signal.freqresp(H_s, w=omega)   #this function returns frequency response H of H_s
+w, H = signal.freqresp(s1, w=omega)
 
+plt.figure()
 plt.plot(H.real, H.imag, )
+plt.plot(-1,0, 'ro')
+plt.text(-1,0, "(-1,0)")
+plt.plot(-0.3, 0, 'bo')
+plt.text(-0.3, 0, "(-0.3,0)")
 plt.ylabel("${Im}\{G(s)\}$")
 plt.title("NYQUIST PLOT")
 plt.xlabel("${Re}\{G(s)\}$")
 plt.grid()
-plt.xlim(-1.6, 0.2)
-plt.ylim(-700, 700)
+plt.xlim(-1.3, 0.2)
+plt.ylim(-1, 1)
 
 #if using termux
 plt.savefig('./figs/ee18btech11050_1.eps')
